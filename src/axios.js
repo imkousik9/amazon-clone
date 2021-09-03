f83669e47e583e4018f43a33f822026d588ffd4b
Kousik Manna <45976284+imkousik9@users.noreply.github.com>
2021-09-03 08:25:15 +0530

    url env

--- src/axios.js
+++ src/axios.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  baseURL: 'https://amazon-clone-node-backend.herokuapp.com/api'
-  // "http://localhost:5001/clone-b687e/us-central1/api"
-  // "http://localhost:5001/api"
+  baseURL: process.env.REACT_APP_URL
 });
 
 export default instance;
